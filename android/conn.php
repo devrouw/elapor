42eff0768f86d73e0cce7c73ef07e7ec5ec44765
Novilawati <novilawati@kompas.com>
2021-07-10 11:49:59 +0800

    config koneksi

---
+++ android/conn.php
@@ -0,0 +1,11 @@
+<?php
+ $con = mysqli_connect("localhost","u250888599_elapor_user","Elapor1234");
+ mysqli_select_db($con,"u250888599_elapor");
+
+  if($con == false){
+     echo "not connected";
+ }else{
+     echo "connected";
+ }
+
+?>
