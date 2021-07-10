db29f478a853d413cc3ec4074260f0d4d95539fa
Novilawati <novilawati@kompas.com>
2021-07-10 19:32:25 +0800

    add mailed password

--- android/api.php
+++ android/api.php
@@ -30,44 +30,52 @@ case "daftar":
     $kecamatan = $_POST['kecamatan'];
     $kelurahan = $_POST['kelurahan'];
     $foto_profil = $_POST['foto_profil'];
-    $s = substr(str_shuffle(str_repeat("!@#$%^&*()0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", 6)), 0, 6);
+    $password = $_POST['password'];
+    // $s = substr(str_shuffle(str_repeat("!@#$%^&*()0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", 6)), 0, 6);
 
-    $query = "INSERT INTO tb_user(
+    $query = "BEGIN; 
+    INSERT INTO tb_masyarakat(
         nik,nama_lengkap,tempat_lahir,tgl_lahir,jenis_kelamin,alamat,email,password,no_telpon,kode_pos,kabupaten,kecamatan,kelurahan,foto_profil
     ) VALUES(
-        '$nik','$nama_lengkap','$tempat_lahir','$tanggal_lahir','$jenis_kelamin','$alamat','$email','$s','$no_telepon','$kode_pos','$kabupaten','$kecamatan','$kelurahan','$foto_profil'
-    )";
+        '$nik','$nama_lengkap','$tempat_lahir','$tanggal_lahir','$jenis_kelamin','$alamat','$email','$password','$no_telepon','$kode_pos','$kabupaten','$kecamatan','$kelurahan','$foto_profil'
+    );
+    INSERT INTO tb_akun(
+        email,password,nik
+    ) VALUES (
+        '$email','$password','$nik'
+    );
+    COMMIT;";
 
-    $hasil = mysqli_multi_query($con,$query);
-    if($hasil){
-        $response["code"] = 200;
-        $response["status"] = "OK";
-        $response["data"] = "data berhasil diinput.";
-        $response["message"] = $message;
-        $subject = 'Akun Anda Berhasil dibuat';
-        echo json_encode($response);
+    // $hasil = mysqli_multi_query($con,$query);
+    // if($hasil){
+    //     $response["code"] = 200;
+    //     $response["status"] = "OK";
+    //     $response["data"] = "data berhasil diinput.";
+    //     $response["message"] = $message;
+    //     $subject = 'Akun Anda Berhasil dibuat';
+    //     echo json_encode($response);
 
-        $message = 'Selamat akun anda telah berhasil dibuat! <br>Sekarang anda bisa mengakses akun anda dengan informasi sbb:<br>
-            email: '.$email. ' <br>password: '.$s.'';
-        $headers = 'From: info@sha-dev.com'       . "\r\n" .
-                    'Reply-To: info@sha-dev.com' . "\r\n" .
-                    'X-Mailer: PHP/' . phpversion();
+    //     $message = 'Selamat akun anda telah berhasil dibuat! <br>Sekarang anda bisa mengakses akun anda dengan informasi sbb:<br>
+    //         Email: '.$email. ' <br>Password: '.$s.'';
+    //     $headers = 'From: info@sha-dev.com'       . "\r\n" .
+    //                 'Reply-To: info@sha-dev.com' . "\r\n" .
+    //                 'X-Mailer: PHP/' . phpversion();
 
-        mail($email, $subject, $message, $headers);
-    }else
-    {
-        $response["code"] = 404;
-        $response["status"] = "error";
-        $response["data"] = null;
-        $response["message"] = "input error $message";
+    //     mail($email, $subject, $message, $headers);
+    // }else
+    // {
+    //     $response["code"] = 404;
+    //     $response["status"] = "error";
+    //     $response["data"] = null;
+    //     $response["message"] = "input error $message";
         
-        echo json_encode($response);
+    //     echo json_encode($response);
 
-    }
+    // }
 
     $message = 'Data Berhasil Diinput!';
     
-    // include './res.php';
+    include './res.php';
 die();
 break;
 
