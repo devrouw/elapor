a1d0046dbb60c3907f6da128be75f1f5c6dbc1b5
Novilawati <novilawati@kompas.com>
2021-07-13 23:50:11 +0800

    add box in beranda

--- _halaman/beranda.php
+++ _halaman/beranda.php
@@ -4,5 +4,23 @@ $judul = $title;
 $setTemplate = true;
 ?>
 <?= content_open('Halaman Beranda') ?>
-Selamat Datang <?=$session->get("dinas")?> di Beranda
-<?= content_close() ?>
+Selamat Datang <?= $session->get("dinas") ?> di Beranda
+
+<div class="row">
+    <div class="col-lg-3 col-6">
+        <!-- small card -->
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>150</h3>
+
+                <p>New Orders</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-shopping-cart"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div>
+    <?= content_close() ?>
