7fb6041b73fa37e2a8c35b1a9f7e73bccbc6677a
Novilawati <novilawati@kompas.com>
2021-07-14 15:21:12 +0800

    add beranda button

--- _halaman/beranda.php
+++ _halaman/beranda.php
@@ -4,15 +4,18 @@ $judul = $title;
 $setTemplate = true;
 ?>
 <?= content_open('Halaman Beranda') ?>
-<h3> <?= $session->get("dinas") ?><h3><br>
-<h3>Kabupaten Penajam Paser</h3>
+<h4 style="text-align: center"> <?= $session->get("dinas") ?><h4>
+<h4 style="text-align: center">Kabupaten Penajam Paser</h4>
+<br><br><br>
 <!-- Boxes untuk data aduan -->
-<div class="row">
+<div class="row align-items-center">
+    <!--<div class="col-lg-1 "></div>-->
+    <div class="col-md-auto"></div>
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-aqua">
+          <div class="small-box bg-aqua text-center">
             <div class="inner">
-            <i class="fa fa-bell"></i>
+            <i class="fa fa-bell" style="font-size: 60px;"></i>
               <p>Pengaduan Masuk</p>
             </div>
             <!-- <div class="icon">
@@ -24,14 +27,10 @@ $setTemplate = true;
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-green">
+          <div class="small-box bg-green text-center">
             <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
-
+                <i class="fa fa-ban" style="font-size: 60px;"></i>
               <p>Belum ditangani</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-stats-bars"></i>
             </div>
             <a href="#" class="small-box-footer">Selengkapnya<i class="fa fa-arrow-circle-right"></i></a>
           </div>
@@ -39,16 +38,15 @@ $setTemplate = true;
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-yellow">
+          <div class="small-box bg-yellow text-center">
             <div class="inner">
-              <h3>44</h3>
-
+                <i class="fa fa-check-circle" style="font-size: 60px;"></i>
               <p>Sudah ditangani</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
             </div>
             <a href="#" class="small-box-footer">Selengkapnya <i class="fa fa-arrow-circle-right"></i></a>
           </div>
+        </div>
+        <div class="col-md-auto"></div>
+        <!--<div class="col-lg-2"></div>-->
         </div>
     <?= content_close() ?>
