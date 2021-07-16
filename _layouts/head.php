35d34cd52adfb0ab59304f0bbaaf91cbc9bc30b4
Novilawati <novilawati@kompas.com>
2021-07-16 18:20:57 +0800

    ganti path foto

--- _layouts/head.php
+++ _layouts/head.php
@@ -1,7 +1,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?=isset($title)?$title:'SIP'?></title>
+  <title><?=isset($title)?$title:'E-Lapor'?></title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
